2e25b32c568e04433bac0d5d432e3f9dd191f0cb
INTI0383 <INTI0383@DESKTOP-BP5GMJ5.home>
2017-08-30 17:34:18 +0200

    comit IDStock et Magasin

--- src/main/java/fr/adaming/springbootangular/model/IdStock.java
+++ src/main/java/fr/adaming/springbootangular/model/IdStock.java
@@ -1,9 +1,38 @@
 package fr.adaming.springbootangular.model;
 
+import javax.persistence.Column;
+import javax.persistence.Embeddable;
+
+@Embeddable
 public class IdStock {
 
+	private Long idP;
+	private Long idB;
+
 	public IdStock() {
-		// TODO Auto-generated constructor stub
+	}
+
+	public IdStock(Long idP, Long idB) {
+		this.idP = idP;
+		this.idB = idB;
+	}
+
+	@Column
+	public Long getIdP() {
+		return idP;
+	}
+
+	public void setIdP(Long idP) {
+		this.idP = idP;
+	}
+
+	@Column
+	public Long getIdB() {
+		return idB;
+	}
+
+	public void setIdB(Long idB) {
+		this.idB = idB;
 	}
 
 }
--- src/main/java/fr/adaming/springbootangular/model/Magasin.java
+++ src/main/java/fr/adaming/springbootangular/model/Magasin.java
@@ -1,12 +1,41 @@
 package fr.adaming.springbootangular.model;
 
+import java.util.ArrayList;
+import java.util.List;
+
+import javax.persistence.DiscriminatorValue;
 import javax.persistence.Entity;
+import javax.persistence.ManyToMany;
 
 
+@DiscriminatorValue(value = "Magasin")
 public class Magasin extends Batiment {
+	
+	private List<Entrepot> listEntrepot = new ArrayList<>();
+	
 
 	public Magasin() {
-		// TODO Auto-generated constructor stub
+		super();
 	}
 
+
+	public Magasin(List<Entrepot> listEntrepot) {
+		super();
+		this.listEntrepot = listEntrepot;
+	}
+
+	
+	@ManyToMany(mappedBy="listMagasin")
+	public List<Entrepot> getListEntrepot() {
+		return listEntrepot;
+	}
+
+
+	public void setListEntrepot(List<Entrepot> listEntrepot) {
+		this.listEntrepot = listEntrepot;
+	}
+
+	
+	
+	
 }
