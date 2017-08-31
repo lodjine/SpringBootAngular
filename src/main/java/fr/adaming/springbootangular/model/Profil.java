bd10ad551baeb4946de8d6e88c267ff8b3cb7504
INTI-0386 <INTI-0386@DESKTOP-3OE7RCC.home>
2017-08-31 09:54:59 +0200

    pojo + annotations de Produit et Profil

--- src/main/java/fr/adaming/springbootangular/model/Profil.java
+++ src/main/java/fr/adaming/springbootangular/model/Profil.java
@@ -1,12 +1,51 @@
 package fr.adaming.springbootangular.model;
 
+import java.util.List;
+
+import javax.persistence.Column;
 import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.ManyToMany;
 
 @Entity
 public class Profil {
+	
+	private Long idProfil;
+	private String role;
+	private List<User> listUser;
 
 	public Profil() {
-		// TODO Auto-generated constructor stub
+	}
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	@Column(unique = true, nullable = false)
+	public Long getIdProfil() {
+		return idProfil;
+	}
+
+	public void setIdProfil(Long idProfil) {
+		this.idProfil = idProfil;
+	}
+
+	@Column
+	public String getRole() {
+		return role;
+	}
+
+	public void setRole(String role) {
+		this.role = role;
+	}
+
+	@ManyToMany(mappedBy="listProfil")
+	public List<User> getListUser() {
+		return listUser;
+	}
+
+	public void setListUser(List<User> listUser) {
+		this.listUser = listUser;
 	}
 
 }
