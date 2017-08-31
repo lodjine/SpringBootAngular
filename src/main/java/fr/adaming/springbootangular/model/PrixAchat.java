0a73042628ad6a34df33e208ef09c7a65b9d1e67
INTI0383 <INTI0383@DESKTOP-BP5GMJ5.home>
2017-08-31 10:57:01 +0200

    PrixAchat

--- src/main/java/fr/adaming/springbootangular/model/PrixAchat.java
+++ src/main/java/fr/adaming/springbootangular/model/PrixAchat.java
@@ -1,5 +1,60 @@
 package fr.adaming.springbootangular.model;
 
+import javax.persistence.Column;
+import javax.persistence.EmbeddedId;
+
 public class PrixAchat {
+
+	private IdPrixAchat idPrixAchat;
+	private String prixAchat;
+	private Produit produit;
+	private Fournisseur fournisseur;
+
+	public PrixAchat(String prixAchat, Produit produit, Fournisseur fournisseur) {
+		super();
+		this.prixAchat = prixAchat;
+		this.produit = produit;
+		this.fournisseur = fournisseur;
+	}
+
+	public PrixAchat() {
+		super();
+	}
+
+	@EmbeddedId
+	public IdPrixAchat getIdPrixAchat() {
+		return idPrixAchat;
+	}
+
+	public void setIdPrixAchat(IdPrixAchat idPrixAchat) {
+		this.idPrixAchat = idPrixAchat;
+	}
+
+	@Column
+	public String getPrixAchat() {
+		return prixAchat;
+	}
+
+	public void setPrixAchat(String prixAchat) {
+		this.prixAchat = prixAchat;
+	}
+
+	@Column
+	public Produit getProduit() {
+		return produit;
+	}
+
+	public void setProduit(Produit produit) {
+		this.produit = produit;
+	}
+
+	@Column
+	public Fournisseur getFournisseur() {
+		return fournisseur;
+	}
+
+	public void setFournisseur(Fournisseur fournisseur) {
+		this.fournisseur = fournisseur;
+	}
 
 }
