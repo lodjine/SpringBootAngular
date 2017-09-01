81f990ed27114a9eec41e10b9a5e09c6d140f7b4
INTI-0373 <INTI-0373@DESKTOP-VQ5TJV1.home>
2017-09-01 10:19:30 +0200

    Init dinoApp

--- src/main/webapp/DinoApp/dinoApp.js
+++ src/main/webapp/DinoApp/dinoApp.js
@@ -1,1 +1,15 @@
-Hello DinoWorld
+// app.js
+var routerApp = angular.module('routerApp', [ 'ui.router', 'ngResource' ]);
+
+routerApp.config(function($stateProvider, $urlRouterProvider) {
+	$stateProvider
+
+	// EXEMPLE !! ========================================
+	.state('add', {
+		url : '/add',
+		templateUrl : 'partial-add.html',
+		controller : 'addController'
+
+	})
+
+});
