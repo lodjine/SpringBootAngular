e45d0a187f144d560540aadcd9b4cc3705620594
Lycan <inti0382@DESKTOP-GJRR859.home>
2017-09-04 14:25:09 +0200

    modif du controller magasin

--- src/main/webapp/DinoApp/magasinListController.js
+++ src/main/webapp/DinoApp/magasinListController.js
@@ -6,4 +6,51 @@ magasinListController.$inject = ['$scope','magasinService'];
 	
 		$scope.magasins=magasinService.query();
 	
+		
+		$scope.deleteMagasin=function deleteMagasin(id) {
+			magasinService.delete({id:id}, function(result){$state.reload();})
+				
+		}
+		
+		$scope.saveMagasin=function saveMagasin(Mmagasin) {
+			if(MagasinService.save(magasin)){
+				console.log("Magasin enregistré");
+				$state.reload();				}
+			else{
+				console.log("Echec de l'enregistrement du magasin");
+			}
+			
+		}
+		
+		$scope.updateMagasin=function updateMagasin(magasin) {
+			if(magasinService.put(magasin)){
+				console.log("Magasin updaté");
+				$state.reload();	
+			}
+			else{
+				console.log("Echec de l'update du magasin");
+			}
+			
+		}
+		
+		$scope.getMagasins=function getMagasins() {
+			if(magasinService.query()){
+				console.log("Liste des magasins récupérée");
+			}
+			else{
+				console.log("Echec de la récupération de la liste des magasins");
+			}
+			
+		}
+		
+		$scope.getMagasin=function getMagasin(id) {
+			if(magasinService.get({id:id})){
+				console.log("Magasins récupéré");
+				$state.reload();	
+			}
+			else{
+				console.log("Echec de la récupération du Magasin");
+			}
+			
+		}
 	};
