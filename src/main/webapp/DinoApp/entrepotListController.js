263cc38b5566bdbb669df4d282f7af995bc40487
T-B0T <INTI-0386@DESKTOP-3OE7RCC.home>
2017-09-04 17:22:38 +0200

    rajouts methodes dans entrepotListController.js + modifs tableau et formulaire entrepot.html

--- src/main/webapp/DinoApp/entrepotListController.js
+++ src/main/webapp/DinoApp/entrepotListController.js
@@ -1,9 +1,26 @@
 angular.module('routerApp').controller('entrepotListController', entrepotListController);
 
-entrepotListController.$inject = ['$scope','entrepotService'];
+entrepotListController.$inject = ['$scope','entrepotService', '$state', '$stateParams'];
 
-	function entrepotListController($scope, entrepotService){
+	function entrepotListController($scope, entrepotService, $state, $stateParams){
 	
 		$scope.entrepots=entrepotService.query();
-	
+		$scope.entrepotDialog={};
+		
+		$scope.deleteEntrepot = function(idEntrepot) {
+			entrepotService.delete({id:idEntrepot}, function(result) {
+				$state.reload();
+			})
+		}
+		
+		$scope.selectEntrepot = function(entrepot){
+			$scope.entrepotDialog=angular.copy(entrepot);
+		}
+		
+		$scope.updateEntrepot = function(entrepotDialog) {
+			entrepotService.save(entrepotDialog, function(result) {
+				$scope.entrepots=entrepotService.query();
+				$state.go('entrepots');
+			});
+		}
 	};
